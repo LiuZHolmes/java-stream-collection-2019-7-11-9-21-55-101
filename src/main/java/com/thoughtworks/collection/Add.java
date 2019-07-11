30ef6277f05de47704ef2cc03184f603a60ec5d2
retr0 <395637649@qq.com>
2019-07-11 20:15:58 +0800

    feat:App,CollectionOperator and Filter

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,27 +2,37 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int start = Math.min(leftBorder, rightBorder);
+        int end = Math.max(leftBorder, rightBorder);
+        return IntStream.rangeClosed(start, end).filter(x -> x % 2 == 0).sum();
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int start = Math.min(leftBorder, rightBorder);
+        int end = Math.max(leftBorder, rightBorder);
+        return IntStream.rangeClosed(start, end).filter(x -> x % 2 != 0).sum();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().mapToInt(x -> x * 3 + 2).summaryStatistics();
+        return (int) stats.getSum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(x -> x % 2 != 0 ? x * 3 + 2 : x).collect(Collectors.toList());
+
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().filter(x -> x % 2 != 0).mapToInt(x -> x * 3 + 5).summaryStatistics();
+        return (int) stats.getSum();
     }
 
 //    public double getMedianOfEven(List<Integer> arrayList) {
@@ -30,15 +40,16 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().filter(x -> x % 2 == 0).mapToInt(x->x).summaryStatistics();
+        return stats.getAverage();
     }
 
-    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
-    }
+//    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
+//        throw new NotImplementedException();
+//    }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(x -> x % 2 == 0).distinct().collect(Collectors.toList());
     }
 
 //    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,30 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        boolean reversed = right < left;
+        int begin = reversed ? right : left;
+        int end = reversed ? left : right;
+        return IntStream.rangeClosed(begin,end).boxed().sorted(reversed ? Comparator.reverseOrder() : Comparator.naturalOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        return getListByInterval(left, right).stream().filter(x->x%2==0).collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).filter(x->x%2==0).boxed().collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length-1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
